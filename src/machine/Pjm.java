2db65ff5bb2e9f2dec5e154e52345cc87bb1a582
jiminghghg <jiminbag387@gmail.com>
2023-11-06 15:20:48 +0900

    kh

---
+++ src/machine/Pjm.java
@@ -0,0 +1,37 @@
+package machine;
+
+import java.util.Scanner;
+
+public class Pjm {
+	Scanner sc;
+	int userIndex, itemSort;
+	String[] searchLog;
+	CustomerDTO cDto;
+	ItemDTO iDto;
+	OrderListDTO oDto;
+	private Object dao;
+	
+	//�엫�떆
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/database";
+    private static final String DB_USER = "username";
+    private static final String DB_PASSWORD = "password";
+	//회원정보수정,아이디,
+    public void pjm() {
+    
+    	
+    	//id수정 쿼리문
+    	//	update CustomerDTO 
+    	  //set pw= ?
+    	String str1 = sc.nextLine();
+    	
+    	
+	}
+    	//where id = ?
+    	
+       
+    	
+    	
+    	
+    	
+    }
+}
