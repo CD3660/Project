7bf1be34802567d8f8ae6e2583823528fbbc1081
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2023-11-27 12:58:33 +0900

    asd
    asd

--- app/src/main/java/com/example/and_mini_project/PayActivity.java
+++ app/src/main/java/com/example/and_mini_project/PayActivity.java
@@ -26,6 +26,8 @@ public class PayActivity extends AppCompatActivity {
         int charge = intent.getIntExtra("charge", 0);
         chargeview.setText(charge);
 
+        TextView total_price = findViewById(R.id.total_price);
+        total_price.setText(dao.total()+"");
         Button pay = findViewById(R.id.btn_2);
         pay.setOnClickListener(view -> {
             if(charge>dao.total()){
