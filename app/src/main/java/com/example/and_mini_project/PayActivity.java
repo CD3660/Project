072e9455c5a95badd3b35cd3ab3799275c7527b1
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2023-11-27 11:13:48 +0900

    asd
    asd

--- app/src/main/java/com/example/and_mini_project/PayActivity.java
+++ app/src/main/java/com/example/and_mini_project/PayActivity.java
@@ -2,7 +2,11 @@ package com.example.and_mini_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class PayActivity extends AppCompatActivity {
 
@@ -10,6 +14,19 @@ public class PayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+        Intent intent = getIntent();
+        ArrayList<OrderListVO> oList = (ArrayList<OrderListVO>)intent.getSerializableExtra("oList");
+        PayDAO dao = new PayDAO(oList);
+        dao.payViewList(this);
+
+        if(intent.getIntExtra() == 0){
+
+        }
+
+
+        Button pay = findViewById(R.id.btn_2);
+
+        Button charge = findViewById(R.id.btn_1);
 
 
 
